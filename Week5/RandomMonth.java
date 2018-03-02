07f0894b2bf1b0eed22e7fc7f1e67451b16b1b24
Kyle Liu <kyle116@yahoo.com>
2018-03-02 11:08:00 -0800

    3.4

---
+++ Week5/RandomMonth.java
@@ -0,0 +1,40 @@
+//Written by:           Kyle Liu
+//Assignment:         Assignment info (ex.: Lab03 - Page 109 - #3.4)
+//Class:                   CO SCI 290
+//Date:                    02/27/2018
+//Description:        Random Month
+import java.util.Scanner;
+
+public class RandomMonth {
+    public static void main(String[] args) {
+        int randomMonth = (int) (Math.random() * 12) + 1;
+        switch (randomMonth) {
+            case 1:  System.out.println("The Random month is: January");
+                break;
+            case 2:  System.out.println("The Random month is: February");
+                break;
+            case 3:  System.out.println("The Random month is: March");
+                break;
+            case 4:  System.out.println("The Random month is: April");
+                break;
+            case 5:  System.out.println("The Random month is: May");
+                break;
+            case 6:  System.out.println("The Random month is: June");
+                break;
+            case 7:  System.out.println("The Random month is: July");
+                break;
+            case 8:  System.out.println("The Random month is: August");
+                break;
+            case 9:  System.out.println("The Random month is: September");
+                break;
+            case 10: System.out.println("The Random month is: October");
+                break;
+            case 11: System.out.println("The Random month is: November");
+                break;
+            case 12: System.out.println("The Random month is: December");
+                break;
+            default: System.out.println("The Random month is: Invalid month");
+                break;
+        }
+    }
+}
