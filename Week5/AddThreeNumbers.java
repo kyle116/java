9779c4e6b2b2aa2b1a6590aa167e23919ea9d35c
Kyle Liu <kyle116@yahoo.com>
2018-03-02 09:32:06 -0800

    3.2

---
+++ Week5/AddThreeNumbers.java
@@ -0,0 +1,20 @@
+//Written by:           Kyle Liu
+//Assignment:         Assignment info (ex.: Lab03 - Page 108 - #3.2)
+//Class:                   CO SCI 290
+//Date:                    02/27/2018
+//Description:        Game: add three numbers
+import java.util.Scanner;
+
+public class AddThreeNumbers {
+    public static void main(String[] args) {
+        int number1 = (int)(System.currentTimeMillis() % 10);
+        int number2 = (int)(System.currentTimeMillis() / 7 % 10);
+        int number3 = (int)(System.currentTimeMillis() / 10 % 10);
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is " + number1 + " + " + number2 + " + " + number3 + "? ");
+        int answer = input.nextInt();
+
+        System.out.println(number1 + " + " + number2 + " + " + number3 + " = " + answer + " is " + (number1 + number2 + number3== answer));
+    }
+}
