c829d728ab4f9c20a590c7c22288df934b050d2c
Kyle Liu <kyle116@yahoo.com>
2018-03-05 10:09:20 -0800

    palindrome

---
+++ Week5/Palindrome.java
@@ -0,0 +1,26 @@
+//Written by:           Kyle Liu
+//Assignment:         Assignment info (ex.: Lab03 - Page 110 - #3.12)
+//Class:                   CO SCI 290
+//Date:                    02/27/2018
+//Description:        Palindrome number
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a three-digit integer: ");
+        Integer number = input.nextInt();
+        String numberReversed = "";
+        int digit1 = number/100;
+        int digit2 = (number/10) % 10;
+        int digit3 = number % 10;
+
+        String numStr = number.toString();
+        numberReversed = numberReversed + digit3 + digit2 + digit1;
+        if(numStr.equals(numberReversed)) {
+            System.out.println(number + " is a palindrome");
+        } else {
+            System.out.println(number + " is not a palindrome");
+        }
+    }
+}
