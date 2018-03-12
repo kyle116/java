57720627f73d97ae59659a430debad9c7dba2ecb
Kyle Liu <kyle116@yahoo.com>
2018-03-12 10:58:40 -0700

    conversions

---
+++ Week6/KiloToPounds.java
@@ -0,0 +1,16 @@
+//Written by:           Kyle Liu
+//Assignment:         Assignment info (ex.: LAB05 - Page 192 - #5.3)
+//Class:                   CO SCI 290
+//Date:                    03/13/2018
+//Description:        Conversion from kilograms to pounds
+
+public class KiloToPounds {
+    public static void main(String[] args) {
+        System.out.println("This program shows a table that converts Kilograms to Pounds up to 199 kilograms");
+        final double KILO_TO_POUNDS = 2.2;
+        for(int i = 1; i < 200; i++) {
+            System.out.println("Kilograms Pounds");
+            System.out.println(i + "        " + (KILO_TO_POUNDS * i));
+        }
+    }
+}
---
+++ Week6/MilesToKilometeres.java
@@ -0,0 +1,16 @@
+//Written by:           Kyle Liu
+//Assignment:         Assignment info (ex.: LAB05 - Page 192 - #5.4)
+//Class:                   CO SCI 290
+//Date:                    03/13/2018
+//Description:        Conversion from miles to kilometers
+
+public class MilesToKilometeres {
+    public static void main(String[] args) {
+        System.out.println("This program shows a table that converts Miles to Kilometers up to 10 miles");
+        final double MILES_TO_KM = 1.609;
+        for(int i = 1; i <= 10; i++) {
+            System.out.println("Miles Kilometers");
+            System.out.println(i + "     " + (MILES_TO_KM * i));
+        }
+    }
+}
