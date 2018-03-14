5c66cacb01224e794f055ca422d4f01a9d20da97
Kyle Liu <kyle116@yahoo.com>
2018-03-13 21:53:07 -0700

    day of week

---
+++ Week6/HW/DayOfWeek.java
@@ -0,0 +1,9 @@
+//Written by:           Kyle Liu 
+//Assignment:         Assignment info (ex.: HW05 - Page 112 - #3.21)
+//Class:                   CO SCI 290
+//Date:                    03/13/2018
+//Description:        Science: day of the week
+import java.util.Scanner;
+
+public class DayOfWeek {
+}
